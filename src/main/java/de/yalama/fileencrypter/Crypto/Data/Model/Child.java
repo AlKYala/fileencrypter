f2841b05fdb4f0bfb3841d50475feaa7295726b2
aliyalama@googlemail.com <Gundam1979!>
2021-07-16 18:02:29 +0200

    adding javadoc

--- src/main/java/de/yalama/fileencrypter/Crypto/Data/Model/Child.java
+++ src/main/java/de/yalama/fileencrypter/Crypto/Data/Model/Child.java
@@ -30,30 +30,66 @@ public class Child implements Serializable {
         this.key = new Key();
     }
 
-    private String generateRandomSalt(String toEncrypt) {
-        return String.format("%dAMOGUS%d%d%s", this.hashCode(), "random".hashCode(),
-                this.hashCode() + this.key.hashCode(), toEncrypt);
-    }
-
-    public void encryptAndStore(String toEncrypt) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException, IOException, ClassNotFoundException {
+    /**
+     * Method to run the encryption in a child instance:
+     * 1. calls methods to generate initialization vector and secretKey
+     * 2. Saves a Key instance based on values from 1.
+     * 3. Encrypts content with key from 2. and AES-128, saves it as instance member
+     *
+     * @param toEncrypt The part that has to be encrypted
+     * @throws Exception in place of: NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+     * InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException,
+     * InvalidKeySpecException, IOException, ClassNotFoundException
+     */
+    public void encryptAndStore(String toEncrypt) throws Exception {
         IvParameterSpec spec = CryptoUtil.generateInitializationVector();
         SecretKey sKey = CryptoUtil.generateKey(toEncrypt, this.generateRandomSalt(toEncrypt));
         this.key.setData(sKey, spec);
         this.encryptContent(toEncrypt, "AES/CBC/PKCS5Padding");
     }
 
-    private void encryptContent(String toEncrypt, String algorithm) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException, ClassNotFoundException {
+    /**
+     * Generates salt for encryption based on the part-string it has to encrypt
+     * @param toEncrypt the part this child has to encrypt
+     * @return random generated salt
+     */
+    private String generateRandomSalt(String toEncrypt) {
+        return String.format("%dAMOGUS%d%d%s", this.hashCode(), "random".hashCode(),
+                this.hashCode() + this.key.hashCode(), toEncrypt);
+    }
+
+    /**
+     * Encrypts the string to store in this instance (in Encrypted form)
+     * assigns member variables the values
+     * @param toEncrypt The string to encrypt in this instance
+     * @param algorithm The encryption algorithm
+     * @throws Exception in place of: NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+     * InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException,
+     * InvalidKeySpecException, IOException, ClassNotFoundException
+     */
+    private void encryptContent(String toEncrypt, String algorithm) throws Exception {
         Cipher cipher = Cipher.getInstance(algorithm);
         cipher.init(Cipher.ENCRYPT_MODE, this.key.getSecretKey(), this.key.getIvParameterSpec());
         this.encryptedPart = Base64.getEncoder().encodeToString(cipher.doFinal(toEncrypt.getBytes()));
         this.encryptedLength = this.encryptedPart.length();
     }
 
+    /**
+     * sets the key instance null for safety reasons
+     */
     public void clearKey() {
         this.key = null;
     }
 
-    public String decrypt() throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {
+    /**
+     * Decrypts the encrypted String saved before
+     * Decryption only happens after contents are loaded and values are assigned
+     * @return returns the decrypted (original) string
+     * @throws Exception in place of: IllegalBlockSizeException, InvalidKeyException, BadPaddingException,
+     * NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException,
+     * IOException, ClassNotFoundException
+     */
+    public String decrypt() throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, this.key.getSecretKey(), this.key.getIvParameterSpec());
         return new String(cipher.doFinal(Base64.getDecoder().decode(this.encryptedPart)));
